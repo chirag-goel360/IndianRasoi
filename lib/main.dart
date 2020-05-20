c8bf5a17d20751474d0e469c5d56818a138100c7
Ritik <ritikmiglani488@gmail.com>
2020-05-20 14:04:08 +0530

    Frontpage

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:indiarasoi/Screens/RecipyInfo.dart';
 
 void main() {
   runApp(MyApp());
@@ -11,9 +12,18 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
+<<<<<<< HEAD
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
+=======
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: RecipyInfo(),
+    );
+  }
+}
+>>>>>>> b4bbcf881cf4e5b448bb87f4f797fc564f205e61
